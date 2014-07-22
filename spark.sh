5d81e3fcd108cff189774f52d8278c09d3a5455b
Zilin Du <zilin.du@gmail.com>
2014-07-22 00:20:05 +0000

    rename st*_docker to docker_st*

--- spark.sh
+++ spark.sh
@@ -9,12 +9,13 @@ MASTER_IMAGE="amplab/spark-master:1.0.0"
 WORKER_IMAGE="amplab/spark-worker:1.0.0"
 SHELL_IMAGE="amplab/spark-shell:1.0.0"
 MASTER_NAME="master"
+WORKER_NAME="worker"
 SHELL_NAME="shell"
 
 DOCKER="sudo docker"
 NUM_WORKERS=2
 
-function start_docker() {
+function docker_start() {
   NAME=$1
   IMAGE=$2
   PARAMS=$3
@@ -23,9 +24,8 @@ function start_docker() {
   $DOCKER logs -f $NAME
 }
 
-function stop_docker() {
+function docker_stop() {
   NAME=$1
-  echo $NAME
   $DOCKER stop $NAME && $DOCKER rm $NAME
 }
 
@@ -42,10 +42,10 @@ if [[ "$1" == "" ]]; then
 else
   case "$1" in
   start_master)
-    start_docker $MASTER_NAME $MASTER_IMAGE
+    docker_start $MASTER_NAME $MASTER_IMAGE
     ;;
   stop_master)
-    stop_docker $MASTER_NAME
+    docker_stop $MASTER_NAME
     ;;
   start_worker)
     MASTER_IP=$($DOCKER inspect -f "{{ .NetworkSettings.IPAddress }}" $MASTER_NAME)
@@ -53,8 +53,7 @@ else
       NUM_WORKERS=$2
     fi 
     for i in `seq 1 $NUM_WORKERS`; do
-      WORKER_NAME=worker$i
-      start_docker $WORKER_NAME $WORKER_IMAGE $MASTER_IP
+      docker_start $WORKER_NAME$i $WORKER_IMAGE $MASTER_IP
     done
     ;;
   stop_worker)
@@ -62,16 +61,15 @@ else
       NUM_WORKERS=$2
     fi 
     for i in `seq 1 $NUM_WORKERS`; do
-      WORKER_NAME=worker$i
-      stop_docker $WORKER_NAME
+      docker_stop $WORKER_NAME$i
     done
     ;;
   start_shell)
     MASTER_IP=$($DOCKER inspect -f "{{ .NetworkSettings.IPAddress }}" $MASTER_NAME)
-    start_docker $SHELL_NAME $SHELL_IMAGE $MASTER_IP "-it"
+    docker_start $SHELL_NAME $SHELL_IMAGE $MASTER_IP "-it"
     ;;
   stop_shell)
-    stop_docker $SHELL_NAME
+    docker_stop $SHELL_NAME
     ;;
   fetch_docker_dns)
     wget https://github.com/zilin/docker-dns/releases/download/v0.0.1/docker-dns
